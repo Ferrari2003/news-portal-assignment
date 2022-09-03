3daada08704ad338a063820fbed3c86c084f97a8
unknown <ferrari2003@gmail.com>
2022-09-03 11:20:57 +0600

    new commit

--- js/api-data.js
+++ js/api-data.js
@@ -2,12 +2,16 @@ const loadNews = () =>{
    fetch(`https://openapi.programming-hero.com/api/news/categories`)
    .then(Response => Response.json())
    .then(data => displayNews(data.data.news_category))
+   .catch(erroe => console(erroe))
 }
 
 const displayNews= categories => {
   const newsConatainer =document.getElementById('news-container');
-  categories.forEach(categories => {
-      
+  categories.forEach(categorie => {
+    const newsDiv =document.createElement('button');
+    newsDiv.classList.add('btn');
+    newsDiv.innerText =`${categorie.category_name}`
+    newsConatainer.appendChild(newsDiv);
   });
    
 }
